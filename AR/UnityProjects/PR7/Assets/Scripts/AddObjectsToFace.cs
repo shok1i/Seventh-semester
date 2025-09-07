0b461a5b84df2f10a68c22edbb6c9a4547d6f238
shokii <shilo152004@mail.ru>
2025-09-07 12:40:39 +0300

    AR (Project_Files and Word_Files) PR8 Done

--- AR/UnityProjects/PR7/Assets/Scripts/AddObjectsToFace.cs
+++ AR/UnityProjects/PR7/Assets/Scripts/AddObjectsToFace.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.XR.ARFoundation;
 
 public class AddObjectsToFace : MonoBehaviour
